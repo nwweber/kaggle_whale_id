dfa0c64ef6c11020383a1584d251b5907eeb5803
Niklas Weber <nalderan@gmail.com>
2016-01-15 15:12:58 +0100

    some changes

--- code/imgs2pckl.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-opj = os.path.join
-
-data_dir = opj("..", "224x224")
-img_dir = opj(data_dir, "images")
-pickle_dir = opj(data_dir, "python_pickle")
-
---
+++ code/whale_id_python/imgs2pckl.py
@@ -0,0 +1,19 @@
+__author__ = 'niklas'
+
+import numpy as np
+import os
+opj = os.path.join
+
+data_dir = opj(".." , "..", "224x224")
+img_dir = opj(data_dir, "images")
+pickle_dir = opj(data_dir, "python_pickle")
+
+img_dir = opj("..", "..", "image_subset")
+
+np_data_array = np.
+
+for image in img_dir:
+    load image
+    append data to np array
+
+pickle np array
